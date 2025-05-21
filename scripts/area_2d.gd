4d6407319a457f26620b7dea3129a24c9d7f0c9b
Benschi006 <mallaribenjaming@gmail.com>
2025-05-21 09:30:13 +0200

    bruh

--- scripts/area_2d.gd
+++ scripts/area_2d.gd
@@ -15,6 +15,6 @@ func _on_body_entered(body: Node2D) -> void:
 func _on_body_exited(body: Node2D) -> void:
 	e_key.hide()
 
-func _process(delta):
+func _physics_process(delta):
 	if entered == true and Input.is_action_just_pressed("E"):
 		icon.show()
