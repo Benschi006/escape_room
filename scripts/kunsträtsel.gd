d133d8b0fbf6a76671231f992788b1c548022586
LeoZe700 <leopold.zeck.1@gmail.com>
2025-05-30 12:43:43 +0200

    kunsträtsel

--- scripts/kunsträtsel.gd
+++ scripts/kunsträtsel.gd
@@ -1,23 +1,35 @@
 extends Area2D
 var entered = false
+
+@onready var icon: Sprite2D = $Icon
 @onready var e_key: Sprite2D = $EKey
-@onready var dunkel: Sprite2D = $Dunkel
+@onready var kunsträtsel: Node2D = $kunsträtsel
+
+
+
 
 func _ready() -> void:
 	e_key.hide()
-	dunkel.hide()
+	icon.hide()
+	kunsträtsel.hide()
+
 func _on_body_entered(body: Node2D) -> void:
 	e_key.show()
 	entered = true
+	
 func _on_body_exited(body: Node2D) -> void:
 	e_key.hide()
 	entered = false
+
 func testE():
 	if Input.is_action_just_pressed("E") and entered == true:
-		dunkel.show()
+		icon.show()
+		kunsträtsel.show()
 func testEsc():
 	if Input.is_action_just_pressed("esc") and entered == true:
-		dunkel.hide()
+		icon.hide()
+		kunsträtsel.show()
 
 func _process(delta):
 	testE()
+	testEsc()
