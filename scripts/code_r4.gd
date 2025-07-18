408f4d4526ee3f7697def29d3db282fb040a7793
Benschi006 <mallaribenjaming@gmail.com>
2025-07-18 13:06:57 +0200

    bruh

--- scripts/code_r4.gd
+++ scripts/code_r4.gd
@@ -1,10 +1,12 @@
 extends Area2D
 
 var entered = false
+var gefüttert = false
 
 @onready var schliessen: Button = $"../schliessen"
 @onready var e_key_2: Sprite2D = $"../EKey2"
 @onready var code_r_4: Sprite2D = $"../CodeR4"
+@onready var nehmen: Button = $"../../safe/nehmen"
 
 
 func _on_body_entered(body: Node2D) -> void:
@@ -14,13 +16,18 @@ func _on_body_entered(body: Node2D) -> void:
 
 func _on_body_exited(body: Node2D) -> void:
 	entered = false
-	e_key_2.hide()
+	if gefüttert == false:
+		e_key_2.hide()
 
 func _on_schliessen_pressed() -> void:
 	schliessen.hide()
 	code_r_4.hide()
 
 func _process(delta: float) -> void:
-	if entered == true and Input.is_action_just_pressed("E"):
+	if entered == true and Input.is_action_just_pressed("E") and gefüttert == true:
 		schliessen.show()
 		code_r_4.show()
+
+
+func _on_nehmen_pressed() -> void:
+	gefüttert = true
